eefbd5ffadd5e2ea60b2161b82ed337e5868dfc4
Shang-Lin Chen <schen@gps.caltech.edu>
2013-09-30 19:02:14 -0700

    Added ignore list to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,24 @@
 # Installs dotfiles in home directory.
 
 dotfiles=${HOME}/dotfiles
+cmd=`basename $0`
+ignore="$cmd .git . .."
 
-for dotfile in `ls $dotfiles`; do
+echo "Changing directories to $dotfiles"
+cd $dotfiles
+
+for dotfile in `ls -a`; do
+  #if [ "$dotfile" == "." -o "$dotfile" == ".." -o "$dotfile" == "$cmd" ]; then
+  if [[ $ignore =~ $dotfile ]]; then
+    echo "Ignoring $dotfile"
+    continue
+  fi
   # Backup existing version of dotfile.
-  if [ -f "${HOME}/${dotfile}" ]; then
+  echo "Backing up ${dotfile}"
+  if [ -e "${HOME}/${dotfile}" ]; then
     mv ${HOME}/${dotfile} ${HOME}/${dotfile}.bak
   fi
+
+  echo "Symlink ${HOME}/${dotfile} => ${dotfile}"
   ln -s ${dotfiles}/${dotfile} ${HOME}/${dotfile}
 done

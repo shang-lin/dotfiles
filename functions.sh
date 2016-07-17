dfa17a52a71cccf19d19e7a825b5d7108a5c045b
Shang-Lin Chen <shanglin.chen@gmail.com>
2016-07-17 10:50:14 -0700

    Created .cheat directory for use with the "cheat" Python package. Added .empty to make sure the directory is added to the repository even if nothing is there.

--- functions.sh
+++ functions.sh
@@ -17,3 +17,17 @@ function comparedir() {
     diff $f ${directory}/$f
     done
 }
+
+# Uses Perl to change Windows line endings to Unix line endings.
+# Backs up the original file as filename.bak and temporarily
+# saves the changed file as filename.tmp.
+function win2unix() {
+  filename=$1
+  if [ -z $1 ]; then
+    echo "No argument provided."
+  else
+    cp ${filename} ${filename}.bak
+    perl -pe 's/\r\n/\n/g' ${filename} >${filename}.tmp
+    mv ${filename}.tmp ${filename}
+  fi
+}
